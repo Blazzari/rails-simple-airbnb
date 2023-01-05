97767ad56eba3d301103242cf7e9ca4af368ed61
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-05 22:16:33 +0100

    commit design

--- Gemfile
+++ Gemfile
@@ -45,6 +45,11 @@ gem "bootsnap", require: false
 # Use Sass to process CSS
 # gem "sassc-rails"
 
+gem "simple_form", github: "heartcombo/simple_form"
+
+# instal faker
+gem 'faker'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+10.times do
+  flat = Flat.new(
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Lorem.paragraph(sentence_count: 6),
+    price_per_night: rand(30..150),
+    number_of_guests: rand(1..8)
+  )
+  flat.save!
+  puts "created #{flat.name} !"
+end
+
+puts 'Finished!'
